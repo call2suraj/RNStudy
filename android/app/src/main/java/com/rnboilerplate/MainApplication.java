02cc68eacc86b36de0843cb806a0b4839962217e
Suraj Behera <call2suraj@gmail.com>
2023-06-07 13:24:40 +1000

    first commit

--- android/app/src/main/java/com/rnboilerplate/MainApplication.java
+++ android/app/src/main/java/com/rnboilerplate/MainApplication.java
@@ -1,4 +1,4 @@
-package com.rnboilerplate;
+package com.RNStudy;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/RNBoilerPlateTests/RNBoilerPlateTests.m
+++ ios/RNBoilerPlateTests/RNBoilerPlateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RNBoilerPlateTests : XCTestCase
+@interface RNStudyTests : XCTestCase
 
 @end
 
-@implementation RNBoilerPlateTests
+@implementation RNStudyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
